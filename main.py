a2695f7f992c77b123da29c535fdb2742b72d6b5
Dave <dave@dwedigital.com>
2024-10-03 18:30:05 +0100

    Squash merge dev into main

--- main.py
+++ main.py
@@ -2,46 +2,48 @@ import soco
 import sys
 import argparse
 
+class Speaker:
+    def __init__(self, name:str="Sonos Move"):
+        self.sonos = self.getSpeaker(name)
 
-def getSpeaker():
-    # Get the Sonos speaker
-    for zone in soco.discover():
-        if zone.player_name == "Sonos Move":
-            sonos = zone
-            break
-    return sonos
+    def getSpeaker(self, name:str):
+        # Get the Sonos speaker
+        for zone in soco.discover():
+            if zone.player_name == name:
+                sonos = zone
+                break
+        return sonos
 
+    def changeVolume(self, volume:int):
+        # Change the volume of the speaker
+        self.sonos.volume = volume
 
-def changeVolume(sonos, volume:int):
-    # Change the volume of the speaker
-    sonos.volume = volume
+    def main(self, volume:int, event:str=None):
+        if volume:
+            self.changeVolume(volume)
 
-
-def main(volume:int=3, event:str=None):
-    sonos = getSpeaker()
-    changeVolume(sonos, volume)
-
-    if event:
-        match event:
-            case "play":
-                sonos.play()
-            case "stop":
-                sonos.stop()
-            case "pause":
-                sonos.pause()
-            case "next":
-                sonos.next()
-            case "previous":
-                sonos.previous()
-            case _:
-                print("Invalid event")
-                sys.exit(1)
+        if event:
+            match event:
+                case "play":
+                    self.sonos.play()
+                case "stop":
+                    self.sonos.stop()
+                case "pause":
+                    self.sonos.pause()
+                case "next":
+                    self.sonos.next()
+                case "previous":
+                    self.sonos.previous()
+                case _:
+                    print("Invalid event")
+                    sys.exit(1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--volume", type=int, default=3)
-    parser.add_argument("-e", "--event", type=str, default=None)
+    parser.add_argument("-v", "--volume", type=int, help="Set the volume level (0-100)")
+    parser.add_argument("-e", "--event", type=str, choices=["play", "stop", "pause", "next", "previous"], help="Control playback events")
     args = parser.parse_args()
 
-    main(args.volume, args.event)
+    speaker = Speaker()
+    speaker.main(args.volume, args.event)
