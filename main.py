48c186db75f3a3f93789535346011187623f50e2
dwedigital <dave@dwedigital.com>
2024-10-04 10:42:36 +0100

    Add readme & rename private class methods

--- main.py
+++ main.py
@@ -2,25 +2,17 @@ import soco
 import sys
 import argparse
 
+import soco.events
+import soco.music_services
+
+
 class Speaker:
-    def __init__(self, name:str="Sonos Move"):
-        self.sonos = self.getSpeaker(name)
+    def __init__(self, name: str = "Sonos Move"):
+        self.sonos = self.__getSpeaker__(name)
 
-    def getSpeaker(self, name:str):
-        # Get the Sonos speaker
-        for zone in soco.discover():
-            if zone.player_name == name:
-                sonos = zone
-                break
-        return sonos
-
-    def changeVolume(self, volume:int):
-        # Change the volume of the speaker
-        self.sonos.volume = volume
-
-    def main(self, volume:int, event:str=None):
+    def main(self, volume: int, event: str = None):
         if volume:
-            self.changeVolume(volume)
+            self.__changeVolume__(volume)
 
         if event:
             match event:
@@ -38,11 +30,29 @@ class Speaker:
                     print("Invalid event")
                     sys.exit(1)
 
+    def __getSpeaker__(self, name: str):
+        # Get the Sonos speaker
+        for zone in soco.discover():
+            if zone.player_name == name:
+                sonos = zone
+                break
+        return sonos
+
+    def __changeVolume__(self, volume: int):
+        # Change the volume of the speaker
+        self.sonos.volume = volume
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--volume", type=int, help="Set the volume level (0-100)")
-    parser.add_argument("-e", "--event", type=str, choices=["play", "stop", "pause", "next", "previous"], help="Control playback events")
+    parser.add_argument(
+        "-e",
+        "--event",
+        type=str,
+        choices=["play", "stop", "pause", "next", "previous"],
+        help="Control playback events",
+    )
     args = parser.parse_args()
 
     speaker = Speaker()
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Sonos CLI
+
+This is a simple command line interface for controlling Sonos speakers. It is written in Python and uses the SoCo library to communicate with the speakers.
+
+## Installation
+Poetry is used as the virtual environment manager and package manager. To install the dependencies, run the following command:
+
+```bash
+poetry install
+```
+
+if poetry is not installed, you can install it using the following command:
+
+```bash
+pip install poetry
+```
+
+
+## Usage
+To run the CLI, use the following command:
+
+```bash
+poetry run python main.py [command]
+```
+
+The following commands are available:
+```
+-e, --event:
+play
+pause
+stop
+next
+previous
+
+-v, --volume:
+volume [level]
+```
